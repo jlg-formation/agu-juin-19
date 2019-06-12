250185491e3066ac258a10c3f0d8a1914b2a1718
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-06-12 11:55:13 +0200

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
 import { CreateComponent } from './routes/create/create.component';
+import { SetupComponent } from './routes/setup/setup.component';
+import { AddQuestionComponent } from './routes/add-question/add-question.component';
+import { SavedComponent } from './routes/saved/saved.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'legal', component: LegalComponent },
   { path: 'create', component: CreateComponent },
+  { path: 'setup', component: SetupComponent },
+  { path: 'add-question', component: AddQuestionComponent },
+  { path: 'saved', component: SavedComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './routes/home/home.component';
 import { CreateComponent } from './routes/create/create.component';
 import { LegalComponent } from './routes/legal/legal.component';
+import { SetupComponent } from './routes/setup/setup.component';
+import { AddQuestionComponent } from './routes/add-question/add-question.component';
+import { SavedComponent } from './routes/saved/saved.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CreateComponent,
-    LegalComponent
+    LegalComponent,
+    SetupComponent,
+    AddQuestionComponent,
+    SavedComponent
   ],
   imports: [
     BrowserModule,
