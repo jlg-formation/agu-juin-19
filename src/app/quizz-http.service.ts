6c2ad2a9e1b2c29dea129cc744701db049b58e34
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-06-14 10:47:21 +0200

    ok

--- src/app/quizz-http.service.ts
+++ src/app/quizz-http.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { QuizzService } from './quizz.service';
 import { environment } from 'src/environments/environment';
+import { Quizz } from './quizz';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,20 @@ export class QuizzHttpService extends QuizzService {
 
   constructor(private http: HttpClient) {
     super();
+    this.http.get(environment.serverUrl).subscribe({
+      next: data => {
+        console.log('sync successfull get.');
+        // tslint:disable-next-line: forin
+        for (const name in data) {
+          data[name].__proto__ = Quizz.prototype;
+        }
+        this.list = data;
+        localStorage.setItem('list', JSON.stringify(this.list));
+      },
+      error: e => {
+        window.alert('Sync non fonctionnelle, mode dégradé');
+      }
+    });
   }
 
   saveCurrent() {
